fc1d1fe7ea8b23c9344fbf0ba6b1d92e19ffd015
imamura0807 <timamura8787@gmail.com>
2019-11-01 16:27:17 +0900

    商品詳細ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
-  resources :products, only: [:new]
+  resources :products, only: [:new, :show]
   resources :users, only: [:destroy, :edit, :show]
   resources :tests, only: [:index]
   get  'sign-up' => 'tests#top'
