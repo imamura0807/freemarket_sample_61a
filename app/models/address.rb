1878e3a2fa94fd548d7a89f891910a192f126732
keita0309 <kta0309@gmail.com>
2019-11-01 10:57:31 +0900

    active_hash

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
+
