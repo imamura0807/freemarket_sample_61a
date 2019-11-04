2a93404b1c36a53518141944337641c773cd93c8
ssaakigithub <olmpc.sk1@gmail.com>
2019-11-04 13:42:54 +0900

    step1-step5のルーティングを編集

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,16 @@
 class SignupController < ApplicationController
+  def step1
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def step5
+  end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,19 +7,4 @@ class TestsController < ApplicationController
   
   def login
   end
-  
-  def step1
-  end
-
-  def step2
-  end
-
-  def step3
-  end
-
-  def step4
-  end
-
-  def step5
-  end
 end
