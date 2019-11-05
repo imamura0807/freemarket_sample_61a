feb5feb3bbbbab76683953b54540f844a6338312
keita0309 <kta0309@gmail.com>
2019-11-05 18:54:29 +0900

    修正

--- app/models/month.rb
+++
@@ -1,8 +0,0 @@
-class Month < ActiveHash::Base
-  self.data = [
-    {id: 1, numeral: '1'}, {id: 2, numeral: '2'}, {id: 3, numeral: '3'},
-    {id: 4, numeral: '4'}, {id: 5, numeral: '5'}, {id: 6, numeral: '6'},
-    {id: 7, numeral: '7'}, {id: 8, numeral: '8'}, {id: 9, numeral: '9'},
-    {id: 10, numeral: '10'}, {id: 11, numeral: '11'}, {id: 12, numeral: '12'},
-  ]
-end
--- app/models/year.rb
+++
@@ -1,9 +0,0 @@
-class Year < ActiveHash::Base
-  self.data = [
-    {id: 1, numeral: '2030'}, {id: 2, numeral: '2031'}, {id: 3, numeral: '2032'},
-    {id: 4, numeral: '2033'}, {id: 5, numeral: '2034'}, {id: 6, numeral: '2035'},
-    {id: 7, numeral: '2036'}, {id: 8, numeral: '2037'}, {id: 9, numeral: '2038'},
-    {id: 10, numeral: '2039'}, {id: 11, numeral: '2040'}, {id: 12, numeral: '2041'},
-    {id: 13, numeral: '2042'}, {id: 14, numeral: '2043'}, {id: 15, numeral: '2044'},
-  ]
-end
