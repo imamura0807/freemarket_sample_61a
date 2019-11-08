ec6464f70e1568fd64ccf8d304d00354d244a624
ssaakigithub <olmpc.sk1@gmail.com>
2019-11-08 17:46:43 +0900

    ログイン、ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'purchases/new'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
-  resources :users, only: [:create, :destroy, :edit, :show, :new]
+  resources :users, only: [:create, :destroy, :edit, :show, :new] do
+    collection do
+      get 'top'
+      get 'login'
+    end
+  end
+
   resources :products, only: [:new, :show, :create] do
       collection do
         get 'get_category_children', defaults: { format: 'json' }
@@ -13,16 +18,23 @@ Rails.application.routes.draw do
       end
   end
 
-  resources :users, only: [:destroy, :edit, :show]
-
   resources :tests, only: [:index]
-  resources :cards, only: [:new]
-  get  'sign-up' => 'tests#top'
-  get  'log-in' => 'tests#login'
+
+  resources :cards, only: [:new, :edit, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+
+  # get  'sign-up' => 'tests#top'
+  # get  'log-in' => 'tests#login'
+  # get  'user-confirm'　=> 'users#new'
+
   # 商品購入確認画面 仮のルート
   resources :purchases, only: [:new]
-  get  'sign-up' => 'tests#top'
-  # get  'user-confirm'　=> 'users#new'
+  
   resources :signup do
     collection do
       get 'info'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_092214) do
+ActiveRecord::Schema.define(version: 2019_11_08_024636) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_11_06_092214) do
     t.integer "card_number", null: false
     t.integer "expire_month", null: false
     t.integer "expire_year", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
@@ -122,7 +124,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_092214) do
     t.string "address_building"
     t.integer "address_phone_number"
     t.text "introduce"
-    t.integer "phone_number", null: false
+    t.bigint "phone_number", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
