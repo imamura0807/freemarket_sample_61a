e90cdf53602d7166f5409c733fe392e3e77f6c4e
ssaakigithub <olmpc.sk1@gmail.com>
2019-11-12 17:46:26 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
-  resources :users, only: [:create, :destroy, :edit, :show, :new] do
+  resources :users, only: [:create, :edit, :new] do
     collection do
       get 'top'
-      get 'login'
+      get 'logout'
+      get 'mypage'
     end
   end
 
