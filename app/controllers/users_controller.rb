788385ff1a22c62677f0164789ad84eb0b5b02ef
keita0309 <kta0309@gmail.com>
2019-11-15 16:13:00 +0900

    マイページjs機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,15 @@ class UsersController < ApplicationController
   
 
   def edit
+ 
   end
   
   def show
+    @nickname = current_user.nickname
+    # @user = User.where(id: current_user.id)
   end
 
   def new
-
-    # @address = Address.new(address_params)
   end
 
   def create
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def mypage
+   
   end
 
   def top
@@ -26,8 +28,11 @@ class UsersController < ApplicationController
   def logout
   end
 
-  # private
+  private
 
+  # def user_params
+  #   params.permit(:id)
+  # end
   # def address_params
   #   params[:address].permit(:prefecture)
   #   params[:address].permit(:created_at, :updated_at, :name, :name_kana, :postal_code, :prefecture, :mayor_town, :address, :building_name, :phone_number)
