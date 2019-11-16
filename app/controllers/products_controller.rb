ea5a72fc1d7da1c850c23403492b3b3dabca3232
imamura0807 <timamura8787@gmail.com>
2019-11-16 19:19:11 +0900

    商品編集・削除機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :set_category, only: [:new, :create]
+  before_action :set_category, only: [:new, :create, :edit, :update]
+
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -8,7 +9,6 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.build
-
   end
 
   def get_category_children
@@ -53,8 +53,59 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy if @product.user_id == current_user.id
+    redirect_to root_path
+  end
 
+  
+  def edit
+    @product = Product.find(params[:id])
+    @profit = (@product.price * 0.9).round
+    @fee = @product.price - @profit
+    if @selected_size_type = @product.size_type
+      @size_types_array = [{id: "---", size_type: "---"}]
+      SizeType.find("#{@selected_size_type.id}").siblings.each do |size_type|
+        size_type_hash = {id: "#{size_type.id}", size_type: "#{size_type.size_type}"}
+        @size_types_array << size_type_hash
+      end
+    else
+      @selected_size_type = nil
+    end
+
+
+    @selected_grandchild_category = @product.category
+    @category_grandchildren_array = [{id: "---", name: "---"}]
+    Category.find("#{@selected_grandchild_category.id}").siblings.each do |grandchild| #siblingsで同じ階層の要素をすべて取得
+      grandchildren_hash = {id: "#{grandchild.id}", name: "#{grandchild.name}"}
+      @category_grandchildren_array << grandchildren_hash
+    end
+    @selected_child_category = @selected_grandchild_category.parent
+    @category_children_array = [{id: "---", name: "---"}]
+    Category.find("#{@selected_child_category.id}").siblings.each do |child|
+      children_hash = {id: "#{child.id}", name: "#{child.name}"}
+      @category_children_array << children_hash
+    end
+    @selected_parent_category = @selected_child_category.parent
+    @category_parents_array = [{id: "---", name: "---"}]
+    Category.find("#{@selected_parent_category.id}").siblings.each do |parent|
+      parent_hash = {id: "#{parent.id}", name: "#{parent.name}"}
+      @category_parents_array << parent_hash
+    end
+
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to action: :show
+    else
+      redirect_to action: :edit, notice: "全項目入力できていません"
+    end
+  end
+
+  private
 
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status, :charge_burden, :prefecture, :send_days, :price, :size_type_id, :brand, images_attributes: [:id, :url]).merge(user_id: current_user.id)
@@ -70,5 +121,4 @@ class ProductsController < ApplicationController
       @category_parent_array << parent.name
     end
   end
-
 end
